a01a141cc3c974cc5b915755a74c36903639ba1f
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-06-28 19:28:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,26 +4,26 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    
+
   end
 
   def self.lowest_rating
-    
+
   end
 
   def self.least_popular_show
-    
+
   end
 
   def self.ratings_sum
-    
+
   end
 
   def self.popular_shows
-    
+
   end
 
   def self.shows_by_alphabetical_order
-    
+
   end
 end
